dbe02f66368b87133f6326dd5ba4ed1f2bc71951
Matt Gerassimoff <mgeras@gmail.com>
2006-12-23 08:32:00 -0700

    added gpl block.

--- nord/convert/units.py
+++ nord/convert/units.py
@@ -1,24 +1,6 @@
 #
 # units.py - unit convertion tables and functions
 #
-# Copyright: Matt Gerassimoff 2007
-#
-# This file is part of g2ools.
-#
-# g2ools is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# g2ools is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
 nm1adsrtime = [
      0.50,   0.70,   1.00,   1.30,   1.50,   1.80,   2.10,   2.30,
      2.60,   2.90,   3.20,   3.50,   3.90,   4.20,   4.60,   4.90,
