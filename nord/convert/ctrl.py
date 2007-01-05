b70de0da934038be0a7258faf4a71cfacd652087
Matt Gerassimoff <mgeras@gmail.com>
2007-01-04 17:57:38 -0700

    new notescale model.

--- nord/convert/ctrl.py
+++ nord/convert/ctrl.py
@@ -93,10 +93,48 @@ class ConvNoteVelScal(Convert):
 
   def domodule(self):
     nmm,g2m = self.nmmodule,self.g2module
-    if len(nmm.inputs.Velocity.cables):
-      mix11a = self.addmodule('Mix1-1A',name='Velocity')
-      setv(mix11a.params.On,1)
-      self.connect(mix11a.outputs.Out,g2m.inputs.Note)
-      setv(mix11a.params.Lev,getv(nmm.params.VelocitySensitivity))
-      self.inputs = mix11a.inputs.In,mix11a.inputs.Chain
+
+    nmmp,g2mp = nmm.params, g2m.params
+    l = getv(nmmp.LeftGain)
+    r = getv(nmmp.RightGain)
+    velsens = getv(nmmp.VelocitySensitivity)
+    external = 0
+    for paramnm in ['VelocitySensitivity','LeftGain','RightGain']:
+      param = getattr(nmmp,paramnm)
+      for checknm in ['knob','ctrl','morph']:
+        if getattr(param,checknm) != None:
+          external = 1
+    if not external and (abs(l-24) > 8 or abs(r-24) > 8) and velsens == 0 :
+      setv(g2mp.L,notescale[l][1])
+      setv(g2mp.R,notescale[r][1])
+      return
+
+    setv(g2mp.L,notescale[l][0])
+    setv(g2mp.R,notescale[r][0])
+    levmult1 = self.addmodule('LevMult',name='24db')
+    self.connect(g2m.outputs.Level,g2m.inputs.In)
+    self.connect(g2m.outputs.Level,levmult1.inputs.Mod)
+    self.connect(g2m.outputs.Out,levmult1.inputs.In)
+
+    if velsens == 0:
+      self.outputs[0] = levmult1.outputs.Out
+      return
+
+    mix21b = self.addmodule('Mix2-1B', name='Vel')
+    setv(mix21b.params.Inv2,1)
+    setv(mix21b.params.Lev1,88)
+    setv(mix21b.params.Lev2,15)
+    self.connect(mix21b.inputs.Chain,mix21b.inputs.In1)
+    self.connect(mix21b.inputs.In1,mix21b.inputs.In2)
+    levmult2 = self.addmodule('LevMult',name='')
+    xfade = self.addmodule('X-Fade',name='VelSens')
+    setv(xfade.params.LogLin,1) # Lin
+    self.connect(mix21b.outputs.Out,levmult2.inputs.Mod)
+    self.connect(levmult1.outputs.Out,levmult2.inputs.In)
+    self.connect(levmult2.inputs.In,xfade.inputs.In1)
+    self.connect(levmult2.outputs.Out,xfade.inputs.In2)
+    setv(xfade.params.Mix,velsens)
+    self.params[0] = xfade.params.Mix
+    self.inputs[0] = mix21b.inputs.Chain
+    self.outputs[0] = xfade.outputs.Out
 
