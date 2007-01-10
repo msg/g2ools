725a8c9290b5e48493d730dc8489898126291aa9
Matt Gerassimoff <mgeras@gmail.com>
2007-01-10 11:14:54 -0700

    applied new glide table to Smooth.

--- nord/convert/ctrl.py
+++ nord/convert/ctrl.py
@@ -38,7 +38,7 @@ class ConvSmooth(Convert):
     nmmp,g2mp = nmm.params, g2m.params
 
     setv(g2mp.Glide,1)
-    updatevals(g2mp,['Time'],nm1smoothtime,g2glidetime)
+    setv(g2mp.Time, glide[getv(nmmp.Time))
     
 class ConvPortamentoA(Convert):
   maing2module = 'Glide'
