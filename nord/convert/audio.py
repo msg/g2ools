f7a62d0ceed7d1cd322821e3e629b37573f562df
Matt Gerassimoff <mgeras@gmail.com>
2007-01-21 14:39:34 -0700

    made delay time at max be 64.

--- nord/convert/audio.py
+++ nord/convert/audio.py
@@ -147,7 +147,12 @@ class ConvDelay(Convert):
     # just divide parameters by 2 to get in ball park
     setv(g2mp.Time2,64)
     # fix delay and delaymod
-    setv(g2mp.Time1,getv(nmmp.Time)/2)
+    time = getv(nmmp.Time)
+    if time == 127:
+      time = 64
+    else:
+      time /= 2
+    setv(g2mp.Time1,time)
     setv(g2mp.Time1Mod,getv(nmmp.Modulation)/2)
 
 class ConvSampleNHold(Convert):
