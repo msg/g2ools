66a6374eeaf80d5c2f95bab8ceb2c7efba673c62
Matt Gerassimoff <mgeras@gmail.com>
2008-08-30 16:41:21 -0700

    add category names

---
+++ nord/g2/categories.py
@@ -0,0 +1,39 @@
+#
+# categories.py - g2 category definitions
+#
+# Copyright (c) 2006,2007 Matt Gerassimoff
+#
+# This file is part of g2ools.
+#
+# g2ools is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# g2ools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+G2Categories = {
+  0: 'No Cat',
+  1: 'Acoustic',
+  3: 'Bass',
+  4: 'Classic',
+  5: 'Drum',
+  6: 'Fantasy',
+  7: 'FX',
+  8: 'Lead',
+  9: 'Organ',
+  10: 'Pad',
+  11: 'Piano',
+  12: 'Synth',
+  13: 'Audio In',
+  14: 'User 1',
+  15: 'User 2',
+}
+
