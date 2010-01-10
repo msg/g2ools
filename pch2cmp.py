6715c73181b314901230282bd6110bc4c9310e3a
Matt Gerassimoff <mgeras@gmail.com>
2010-01-09 21:35:29 -0700

    code cleanups.

--- pch2cmp.py
+++ pch2cmp.py
@@ -6,20 +6,15 @@ from nord.g2.file import Pch2File
 
 def cmpnm(a, b):
   x = cmp(a.module.type.shortnm, b.module.type.shortnm)
-  if x:
-    return x
+  if x: return x
   return cmp(a.type.name, b.type.name)
 
 def getnets(pch2):
   def ordernets(a, b):
-    ao = a.output
-    bo = b.output
-    x = cmpnm(ao, bo)
+    x = cmpnm(a.output, b.output)
     if x: return x
-    ains = len(a.inputs)
-    bins = len(b.inputs)
-    if ains != bins:
-      return cmp(ains, bins)
+    x = cmp(len(a.inputs), len(b.inputs))
+    if x: return x
     for i in range(len(a.inputs)):
       x = cmpnm(a.inputs[i], b.inputs[i])
       if x: return x
@@ -35,28 +30,26 @@ def chkports(modmap, aport, bport):
   x = cmpnm(aport, bport)
   if x: return x
   # if index of aport.module not seen, create a new mapping to bport.module
-  idx = aport.module.index
-  if modmap.has_key(idx):
-    return cmp(modmap[idx], bport.module.index)
-  modmap[idx] = bport.module.index
+  aidx, bidx = aport.module.index, bport.module.index
+  if modmap.has_key(aidx):
+    return cmp(modmap[aidx], bidx)
+  modmap[idx] = bidx
   return 0 # only have one to check against, have to assume it's correct.
 
 def cmpnetlist(a, b):
   x = cmp(len(a), len(b))
   if x: return x
   modmap = {}
-  for i in range(len(a)):
-    anet = a[i]
-    bnet = b[i]
+  for anet, bnet in zip(a, b):
     # output module/port must match
     x = chkports(modmap, anet.output, bnet.output)
     if x: return x
     # length of netlist must match
     x = cmp(len(anet.inputs),len(bnet.inputs))
     if x: return x
-    for j in range(len(anet.inputs)):
-      # input module/port must match
-      x = chkports(modmap, anet.inputs[j],bnet.inputs[j])
+    # all inputs must match
+    for ain, bin in zip(anet.inputs, bnet.inputs):
+      x = chkports(modmap, ain, bin)
       if x: return x
   return 0
     
